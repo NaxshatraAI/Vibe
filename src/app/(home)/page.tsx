af087080c52a1bed8f206a193f9f86d6df0e18ce
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-12-08 23:51:12 +0530

    Clean up formatting in page.tsx and remove unused imports

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,14 @@
 import { ProjectForm } from "@/modules/home/ui/components/project-form";
 import { ProjectsList } from "@/modules/home/ui/components/projects-list";
-import Image from "next/image";
 import { AuroraHero } from "@/modules/home/ui/components/aurora-hero";
-
 
 const Page = () => {
   return (
- 
     <AuroraHero>
-          <ProjectForm />
-       
+      <ProjectForm />
       <ProjectsList />
     </AuroraHero>
-
-   
-  )
-}
+  );
+};
 
 export default Page;
